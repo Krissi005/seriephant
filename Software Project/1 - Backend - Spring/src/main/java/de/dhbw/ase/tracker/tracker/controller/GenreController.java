bf678586981046ed03508ddae22b52d4d5287b0e
Krissi <kristin.agne@ptvgroup.com>
2022-02-07 20:55:01 +0100

    :sparkles: Added controller for genre

---
+++ Software Project/1 - Backend - Spring/src/main/java/de/dhbw/ase/tracker/tracker/controller/GenreController.java
@@ -0,0 +1,64 @@
+package de.dhbw.ase.tracker.tracker.controller;
+
+import de.dhbw.ase.tracker.tracker.model.Genre;
+import de.dhbw.ase.tracker.tracker.service.GenreService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/genre")
+@RequiredArgsConstructor
+public class GenreController {
+    private final GenreService genreService;
+    /************************************************************************************************************************************/
+
+    /*
+         _____                _
+        / ____|              | |
+        | |     _ __ ___  __ _| |_ ___
+        | |    | '__/ _ \/ _` | __/ _ \
+        | |____| | |  __/ (_| | ||  __/
+        \_____|_|  \___|\__,_|\__\___|
+     */
+
+    /************************************************************************************************************************************/
+    /*
+         _____                _
+        |  __ \              | |
+        | |__) |___  __ _  __| |
+        |  _  // _ \/ _` |/ _` |
+        | | \ \  __/ (_| | (_| |
+        |_|  \_\___|\__,_|\__,_|
+    */
+
+    @GetMapping(value = "")
+    public List<Genre> getAllGenres(){
+        return genreService.getAllGenres();
+    }
+
+    /************************************************************************************************************************************/
+    /*
+         _    _           _       _
+        | |  | |         | |     | |
+        | |  | |_ __   __| | __ _| |_ ___
+        | |  | | '_ \ / _` |/ _` | __/ _ \
+        | |__| | |_) | (_| | (_| | ||  __/
+        \____/| .__/ \__,_|\__,_|\__\___|
+              | |
+              |_|
+    */
+    /************************************************************************************************************************************/
+    /*
+         _____       _      _
+        |  __ \     | |    | |
+        | |  | | ___| | ___| |_ ___
+        | |  | |/ _ \ |/ _ \ __/ _ \
+        | |__| |  __/ |  __/ ||  __/
+        |_____/ \___|_|\___|\__\___|
+    */
+    /************************************************************************************************************************************/
+}
