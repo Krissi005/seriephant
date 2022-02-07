b2ef491ab24a2798eb7fea6a39170b5b28c48bb9
Krissi <kristin.agne@ptvgroup.com>
2022-02-07 20:55:16 +0100

    :sparkles: Added DataInserter

---
+++ Software Project/1 - Backend - Spring/src/main/java/de/dhbw/ase/tracker/tracker/DataInserter.java
@@ -0,0 +1,28 @@
+package de.dhbw.ase.tracker.tracker;
+
+import de.dhbw.ase.tracker.tracker.model.Genre;
+import de.dhbw.ase.tracker.tracker.repository.GenreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class DataInserter implements ApplicationListener<ApplicationReadyEvent> {
+
+    @Autowired
+    GenreRepository genreRepository;
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        Genre genre = new Genre("Horror", "Löst Gefühle der Angst, des Schreckens und Verstörung aus.");
+        genreRepository.save(genre);
+        genre = new Genre("SciFi", "Meist geht es um Zeitreisen, Reisen zu anderen Planeten oder um neuartige Erfindungen");
+        genreRepository.save(genre);
+        genre = new Genre("Thriller", "Es wird eine Spannung erzeugt, die während des gesamten Handlungsverlaufs präsent ist.");
+        genreRepository.save(genre);
+    }
+}
