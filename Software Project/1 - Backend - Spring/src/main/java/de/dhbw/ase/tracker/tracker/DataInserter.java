1a31875cdbf1df92e36c85494aafc12624fba985
Krissi <kristin.agne@ptvgroup.com>
2022-02-09 11:06:21 +0100

    :sparkles: Added demo data for Serie and adapted it for Season

--- Software Project/1 - Backend - Spring/src/main/java/de/dhbw/ase/tracker/tracker/DataInserter.java
+++ Software Project/1 - Backend - Spring/src/main/java/de/dhbw/ase/tracker/tracker/DataInserter.java
@@ -3,9 +3,11 @@ package de.dhbw.ase.tracker.tracker;
 import de.dhbw.ase.tracker.tracker.model.Episode;
 import de.dhbw.ase.tracker.tracker.model.Genre;
 import de.dhbw.ase.tracker.tracker.model.Season;
+import de.dhbw.ase.tracker.tracker.model.Serie;
 import de.dhbw.ase.tracker.tracker.repository.EpisodeRepository;
 import de.dhbw.ase.tracker.tracker.repository.GenreRepository;
 import de.dhbw.ase.tracker.tracker.repository.SeasonRepository;
+import de.dhbw.ase.tracker.tracker.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
@@ -29,6 +31,9 @@ public class DataInserter implements ApplicationListener<ApplicationReadyEvent> 
     @Autowired
     SeasonRepository seasonRepository;
 
+    @Autowired
+    SerieRepository serieRepository;
+
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
         Genre genre = new Genre("Horror", "Löst Gefühle der Angst, des Schreckens und Verstörung aus.");
@@ -38,15 +43,20 @@ public class DataInserter implements ApplicationListener<ApplicationReadyEvent> 
         genre = new Genre("Thriller", "Es wird eine Spannung erzeugt, die während des gesamten Handlungsverlaufs präsent ist.");
         genreRepository.save(genre);
 
-        Season season = new Season(5);
-        seasonRepository.save(season);
+        Serie serie = new Serie("Superman & Lois", "Superman & Lois ist eine US-amerikanische Science-Fiction-Fernsehserie, die auf den gleichnamigen Figuren, Superman und Lois Lane, aus den DC Comics basiert.");
+        serieRepository.save(serie);
 
-        Episode episode1 = new Episode("Rückkehr nach Smallville", 1, season);
+        Season season2 = new Season(2, serie);
+        seasonRepository.save(season2);
+
+        Season season1 = new Season(1, serie);
+        seasonRepository.save(season1);
+
+        Episode episode1 = new Episode("Rückkehr nach Smallville", 1, season1);
         episodeRepository.save(episode1);
-        Episode episode2 = new Episode("Seltsames Verschwinden", 2, season);
+        Episode episode2 = new Episode("Seltsames Verschwinden", 3, season1);
         episodeRepository.save(episode2);
-        List<Episode> episodes = new ArrayList<>();
-        episodes.add(episode1);
-        episodes.add(episode2);
+        Episode episode3 = new Episode("Erbe", 2, season2);
+        episodeRepository.save(episode3);
     }
 }
