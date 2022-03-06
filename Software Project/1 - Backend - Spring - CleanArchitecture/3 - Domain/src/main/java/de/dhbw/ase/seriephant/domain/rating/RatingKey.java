ebcc232f66f9fd1148daae4b77fe773c5f749b92
Krissi <kristin.agne@ptvgroup.com>
2022-03-06 16:28:27 +0100

    :rotating_light: Fixed warning of not implemented methods.

--- Software Project/1 - Backend - Spring - CleanArchitecture/3 - Domain/src/main/java/de/dhbw/ase/seriephant/domain/rating/RatingKey.java
+++ Software Project/1 - Backend - Spring - CleanArchitecture/3 - Domain/src/main/java/de/dhbw/ase/seriephant/domain/rating/RatingKey.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
 @AllArgsConstructor
@@ -19,4 +20,21 @@ public class RatingKey implements Serializable {
 
     @Column(name = "episode_id")
     Long episodeId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+        RatingKey ratingKey = (RatingKey) o;
+        return Objects.equals(this.userId, ratingKey.userId) && Objects.equals(this.episodeId, ratingKey.episodeId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.userId, this.episodeId);
+    }
 }
